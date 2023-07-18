55317490f32291e2a987c9580581ff071655d457
Maxwell Alves Dos Santos <maxdsantos@simpress.com.br>
2023-07-17 21:28:58 -0300

    ajuste fetch produtos

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -76,7 +76,7 @@ export default function ProductsPage(props: any) {
 
 export async function getServerSideProps() {
 
-  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/getProducts`)
+  const response = await fetch(`http://localhost:3000/api/getProducts`)
 
   const parseJson = await response.json()
 
